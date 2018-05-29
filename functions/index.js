bef897b3e77201d7f0c525e06067a49a71db1e8d
alex <alex.r.hancock@googlemail.com>
2018-05-29 16:58:15 +0100

    outline of function to send question to phones/ping them to sync with appropriate database

--- functions/index.js
+++ functions/index.js
@@ -29,3 +29,19 @@ exports.addUser = functions.https.onRequest((req, res) => {
       });
   });
 });
+
+// exports.sendQuestion = functions.firestore
+//   .document("Current_Event/{event_id}")
+//   .onUpdate((change, context) => {
+//     // Get an object representing the document
+//     // e.g. {'name': 'Marie', 'age': 66}
+//     const newValue = change.after.data();
+
+//     // ...or the previous value before this update
+//     const previousValue = change.before.data();
+
+//     // access a particular field as you would any JS property
+//     const name = newValue.name;
+
+//     // perform desired operations ...
+//   });
