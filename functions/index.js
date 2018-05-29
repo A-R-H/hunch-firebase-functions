a210d3658219917e13a19d6b6bfc3eea55bec804
alex <alex.r.hancock@googlemail.com>
2018-05-29 16:56:17 +0100

    initial function to take authenticated user and add them to a collection

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,31 @@
 const functions = require("firebase-functions");
 const firebase = require("firebase");
 require("firebase/firestore");
+const cors = require("cors")();
 const { firebaseConfig } = require("../config");
 
 firebase.initializeApp(firebaseConfig);
 
 const db = firebase.firestore();
+
+exports.addUser = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {
+    const { uid, username, email, creation_time } = req.body;
+    db
+      .collection("Users")
+      .doc(`${uid}`)
+      .set({
+        username,
+        email,
+        creation_time
+      })
+      .then(docRef => {
+        //docRef is undefined??
+        console.log("docRef", docRef);
+        res.send({ docRef, err: null });
+      })
+      .catch(err => {
+        res.send({ err });
+      });
+  });
+});
